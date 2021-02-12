4487632c8caec425a10e418cbe7966003e0b90d1
yusuf <ysfozturk3@gmail.com>
2021-02-12 14:04:39 +0100

    number, string, tupple, array,enum

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+
+function App() {
+
+function greter(name:string){
+  return `hello ${name}`
+}
+/**
+ * values - bir deger girilecekse bu degerin turu belirtilmelidir.
+ *  ama zaten js kodlari da yazilabildiginden bu tur belirtilmese de sorun olmaz
+ */
+
+//number
+let num :number
+num = 5.3
+//string
+let city:string
+city = 'Kassel'
+//boolean
+let isRight: boolean
+isRight = true
+
+//array
+let numbers: number[] = [1,2,3]
+//array with generic -- ayni sonucu aliriz ama bu seklide olusturdugumuz bir formatta degerler alabiliriz.
+let numbers2:Array<number> = [4,5,6]
+
+// bu sekilde 2 ayri veri tipi ayni listede tutulabilir.
+let tupple : [number,string] = [7,'love']
+
+//js gibi yazmak ta mumkun
+let age = 20
+
+// enum - class mantiginda, bu yuzden buyuk harfle yazilir
+
+enum Colors {red=1,black,purple} // 1 baslangic degeri vermek icin, digerleri otomak 2 ve 3 olacak
+enum Colors2 {red='rot',black='schwarz',purple='lila'}
+let color = Colors.red
+let color2 = Colors2.black
+
+  return (
+    <div className="App">
+      <p>{greter('yusuf')}</p>
+      <p>{num} {city} {age} {isRight && 'true'} {numbers} {numbers2} {tupple}</p>
+      <p>{color} {color2}</p>
+    </div>
+  );
+}
+
+export default App;
